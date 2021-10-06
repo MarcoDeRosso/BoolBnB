ecf07a34c88ff36f13abb994043d9a904bd028b8
emmepi92 <mariapiacoccioli@live.it>
2021-10-06 16:37:19 +0200

    prosa

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,88 @@
+<footer class="my_footer">
+    <div class="container p-4">
+        <div class="row">
+            <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                <h3><a class="text-uppercase  mb-0 text-decoration-none" href="">Informazioni</a></h3>
+                <ul class="list-unstyled ">
+                    <li><a href="#" class=" mb-0 text-decoration-none">Come funziona BoolBnb</a></li>
+                    <li><a href="#" class=" mb-0 text-decoration-none">Newsroom</a></li>
+                    <li><a href="#" class=" mb-0 text-decoration-none">BoolBnb 2021</a></li>
+                    <li><a href="#" class=" mb-0 text-decoration-none">BoolBnb Plus</a></li>
+                    <li><a href="#" class=" mb-0 text-decoration-none">BoolBnb Luxe</a></li>
+                    <li><a href="#" class=" mb-0 text-decoration-none">HoteTonight</a></li>
+                    <li><a href="#" class=" mb-0 text-decoration-none">Boolbnb for Work</a></li>
+                    <li><a href="#" class=" mb-0 text-decoration-none">Grazie agli host puoi</a></li>
+                    <li><a href="#" class=" mb-0 text-decoration-none">Lavora con noi</a></li>
+                    <li><a href="#" class=" mb-0 text-decoration-none">Lettera dei fondatori</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-md-6 mb-4 mb-md- 0">
+                <h3> <a class="text-uppercase  mb-0 text-decoration-none" href="">COMMUNITY</a></h3>
+                <ul class="list-unstyled mb-0">
+                    <li><a href="#" class=" mb-0 text-decoration-none">Diversità e appartenenza</a></li>
+                    <li><a href="#" class=" mb-0 text-decoration-none">Accessibilità</a></li>
+                    <li><a href="#" class=" mb-0 text-decoration-none">Boolbnb Associates</a></li>
+                    <li><a href="#" class=" mb-0 text-decoration-none">Ospita i rifugiati afghani</a></li>
+                    <li><a href="#" class=" mb-0 text-decoration-none">Invitare un ospite</a></li>
+                    <li><a href="#" class=" mb-0 text-decoration-none">Boolbnb.org</a></li>
+                </ul>    
+            </div>
+
+            <div class="col-lg-3 col-md-6 mb-4 mb-md- 0">
+                <h3><a class="text-uppercase  mb-0 text-decoration-none" href="">Ospita</a></h3>
+                <ul class="list-unstyled mb-0">
+                    <li><a href="#" class=" mb-0 text-decoration-none">Diventa un host</a></li>
+                    <li><a href="#" class=" mb-0 text-decoration-none">Offri un'Esperienza online</a></li>
+                    <li><a href="#" class=" mb-0 text-decoration-none">Offri un'Esperienza </a></li>
+                    <li><a href="#" class=" mb-0 text-decoration-none">Ospitare responsabilmente</a></li>
+                    <li><a href="#" class=" mb-0 text-decoration-none">Centro Risorse</a></li>
+                    <li><a href="#" class=" mb-0 text-decoration-none">Community Center</a></li>
+                </ul>
+
+            </div>
+            <div class="col-lg-3 col-md-6 mb-4 mb-md- 0">
+                <h3> <a class="text-uppercase  mb-0 text-decoration-none" href="">Assistenza</a></h3>
+                <ul class="list-unstyled mb-0">
+                    <li><a href="#" class=" mb-0 text-decoration-none">La nostra risposta all'emergenza COVID-19</a></li>
+                    <li><a href="#" class=" mb-0 text-decoration-none">Centro Assistenza</a></li>
+                    <li><a href="#" class=" mb-0 text-decoration-none">Opzioni di cancellazione</a></li>
+                    <li><a href="#" class=" mb-0 text-decoration-none">Servizio di supporto al vicinato</a></li>
+                    <li><a href="#" class=" mb-0 text-decoration-none">Affidabilità e sicurezza</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    
+    <div class="for-line-only">
+       <section class="p-3 pt-0 container footer_bottom">
+           <div class="row d-flex align-items-center  justify-content-between">
+               <div class="text-center">
+                   <div class="p-3">
+                      <strong> © 2021 Boolbnb. Team 1  </strong>- 
+                       <a class=" mb-0 text-decoration-none" href="#"> Privacy  </a>
+                       <span>-</span>
+                       <a class=" mb-0 text-decoration-none" href="#"> Termini  </a>
+                       <span>-</span>
+                       <a class=" mb-0 text-decoration-none" href="#"> Mappa del sito  </a>
+                       <span>-</span>
+                       <a class=" mb-0 text-decoration-none" href="#"> Dettagli dell'azienda</a>
+                   </div>
+               </div>
+                <div class="text-center">
+                    <div class="p-3">
+                        <a class=" mb-0 text-decoration-none" href="#"> s.r  </a>
+                        <span>-</span>
+                        <a class=" mb-0 text-decoration-none" href="#"> m.c  </a>
+                        <span>-</span>
+                        <a class=" mb-0 text-decoration-none" href="#"> m.d  </a>
+                        <span>-</span>
+                        <a class=" mb-0 text-decoration-none" href="#"> g.c</a>
+                        <span>-</span>
+                        <a class=" mb-0 text-decoration-none" href="#"> g.a</a>
+                    </div>
+                </div>
+            </div>    
+        </section>
+       
+    </div>
+</footer>

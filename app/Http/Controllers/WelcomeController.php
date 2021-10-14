cd5b6fbcf334b984dacf9fc45253ee20153f3093
Sebushka89 <sebastian.rizzo@hotmail.it>
2021-10-14 13:42:42 +0200

    added apartment at welcome page

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -4,16 +4,30 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\DB;
 use App\Apartment;
+use App\Service;
 
 
 class WelcomeController extends Controller
 {
     public function welcome () {
+         $apartments= Apartment::paginate(6);
+        $services = Service::all();
 
+        $listServicesArray =[];
+        foreach($apartments as $apartment) {
+            $listServices =($apartment->service()->where('apartment_id','=', $apartment->id)->get())->toArray();
+
+            foreach($listServices as $service){
+                $listServicesArray[] = $service['id'];               
+            }
+            $lista[] = $listServicesArray;            
+            $listServicesArray=[];
+        }    
         $apartmentsSponsored = DB::table('payments')
                                 ->leftJoin('apartments','payments.apartment_id', '=', 'apartments.id')
                                 ->where('expire_date', '>', '2021-10-15')
                                 ->get();
-        return view('welcome', compact('apartmentsSponsored'));
+        return view('welcome', compact('apartmentsSponsored', 'apartments'));
     }
+    
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,11 +14,30 @@
     ></slider-card>
     
 </div>
+<div class="container home">
+    <h1>Le mete più gettonate:</h1>
+    <div class="row">
+        @foreach ($apartments as $apartment)
+        <div class="articol-card col-12 col-md-6 col-lg-4 mt-3 mb-3">
+            <a href="{{ route('apartmentShow', $apartment) }}" class="apartment">
+                <h1>{{ $apartment->title }}</h1>
+                <img class="img-apartment mb-3" src="{{ $apartment->img_path }}" alt="" style="width: 100%;">
+                {{-- <div class="text-description">{{ $apartment->description }}</div> --}}
+            </a>      
+        </div>        
+        @endforeach
+    </div>
+
+</div>
+
+<div class="d-flex justify-content-center mt-3">
+    {{ $apartments->links() }}
+</div>
 
 <div class="jumbotron-footer d-flex justify-content-center align-items-center">
     <div class="text-center ">
         <h3><strong>Condividi il tuo spazio per guadagnare qualcosa in più e cogliere nuove opportunità.</strong></h3>
-        <a href="{{ route('register') }}"><button class=" btn btn-primary reg-btn">Diventa Host</button></a>
+        <a href="{{ route('register') }}"><button class=" btn btn-primary reg-btn"> Diventa Host</button></a>
     </div>
 </div>
 @endsection

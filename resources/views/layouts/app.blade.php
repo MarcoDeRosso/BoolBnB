e01146cad794a76cc5f8811409a49568c5a952b0
Sebushka89 <sebastian.rizzo@hotmail.it>
2021-10-19 17:31:08 +0200

    ok

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,7 +29,7 @@
         <nav class="navbar sticky-top navbar-expand-md navbar-light shadow-sm">
             <div class="container ">
                 <a  href="{{ url('/') }}">
-                    {{ config('app.name', 'BoolBnB') }}
+                    <img style="box-shadow: none; border-radius:0; height:60px" src="{{asset('img/logo_boolbnb_blu.png')}}" alt="" srcset="">
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
